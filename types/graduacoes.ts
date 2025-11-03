27a26eb6ecf9be85f839c58bc00d2593a1a78c4a
Manoela Urias <manoela.urias@treetech.com.br>
2025-11-03 17:50:39 -0300

    registro de usuario

---
+++ types/graduacoes.ts
@@ -0,0 +1,49 @@
+export interface GraduacaoMuayThai {
+  cor: string;       
+  pontaBranca?: boolean;
+}
+
+export const graduaçõesMuayThai: GraduacaoMuayThai[] = [
+  { cor: "Amarela" },
+  { cor: "Amarela", pontaBranca: true },
+  { cor: "Verde" },
+  { cor: "Verde", pontaBranca: true },
+  { cor: "Azul" },
+  { cor: "Azul", pontaBranca: true },
+  { cor: "Marrom" },
+  { cor: "Marrom", pontaBranca: true },
+  { cor: "Vermelha" },
+  { cor: "Vermelha", pontaBranca: true },
+  { cor: "Preto" },
+];
+
+
+export interface GraduacaoJiuJitsu {
+  cor: string;    
+  faixa?: string; 
+  grau?: 1 | 2 | 3 | 4;  
+}
+
+
+export const graduaçõesJiuJitsu: GraduacaoJiuJitsu[] = [
+  { cor: "Branca", grau: 1 },
+  { cor: "Branca", grau: 2 },
+  { cor: "Branca", grau: 3 },
+  { cor: "Branca", grau: 4 },
+  { cor: "Azul", grau: 1 },
+  { cor: "Azul", grau: 2 },
+  { cor: "Azul", grau: 3 },
+  { cor: "Azul", grau: 4 },
+  { cor: "Roxa", grau: 1 },
+  { cor: "Roxa", grau: 2 },
+  { cor: "Roxa", grau: 3 },
+  { cor: "Roxa", grau: 4 },
+  { cor: "Marrom", grau: 1 },
+  { cor: "Marrom", grau: 2 },
+  { cor: "Marrom", grau: 3 },
+  { cor: "Marrom", grau: 4 },
+  { cor: "Preta", grau: 1 },
+  { cor: "Preta", grau: 2 },
+  { cor: "Preta", grau: 3 },
+  { cor: "Preta", grau: 4 },
+];
---
+++ types/usuarios.ts
@@ -0,0 +1,24 @@
+// src/types/usuarios.ts
+
+import { GraduacaoJiuJitsu, GraduacaoMuayThai } from "./graduacoes";
+
+export interface Filho {
+  id: string;
+  nome: string;
+  modalidade: "Muay Thai" | "Jiu-Jitsu"; 
+  graduacao: GraduacaoMuayThai | GraduacaoJiuJitsu;
+  observacao?: string;
+  dataDeRegistro: string;
+}
+
+export interface Usuario {
+  id: string;
+  nome: string;
+  modalidade: "Muay Thai" | "Jiu-Jitsu"; 
+  graduacao: GraduacaoMuayThai | GraduacaoJiuJitsu;
+  observacao?: string;
+  email: string;
+  telefone: string;
+  dataDeRegistro: string;
+  filhos?: Filho[]; 
+}
